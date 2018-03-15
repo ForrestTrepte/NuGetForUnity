e2ebeb3e4793cda9e9bd4692006d3cbec2e7734e
Forrest Trepte <forrest@microsoft.com>
2018-03-15 14:15:12 -0700

    PowerShell script that copies the .dlls from a NuGetForUnity build and exports a new .unitypackage

---
+++ Packager/UpdateAndExportPackage.ps1
@@ -0,0 +1,12 @@
+﻿param (
+    [string]$UnityExePath,
+    [string]$NuGetForUnityBuildDirectory)
+
+# Copy .dlls from the build into the NuGetEditor folder
+$BuildFiles = Join-Path -Path $NuGetForUnityBuildDirectory -ChildPath "*.dll"
+$DestinationFolder = Join-Path -Path $PSScriptRoot -ChildPath "Assets\NuGet\Editor"
+Copy-Item $BuildFiles -Destination $DestinationFolder
+
+# Launch Unity to export the NuGetForUnity package
+$PackageFileName = Join-Path -Path $PSScriptRoot -ChildPath "NuGetForUnity.unitypackage"
+Start-Process -FilePath $UnityExePath -ArgumentList "-batchMode -quit -projectPath $PSScriptRoot -exportPackage Assets/NuGet $PackageFileName"
